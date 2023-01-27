b52d160b23aef3df2923f123fc4057c8600e1580
pildspalva <123443302+pildspalva@users.noreply.github.com>
2023-01-27 09:04:54 +0200

    komentars 2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace RS_27_01_2023_jap
     {
         static void Main(string[] args)
         {
+            {
+                int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+                for (int i = 0; i < numbers.GetLength(0); i++)
+                {
+                    for (int j = 0; j < numbers.GetLength(1); j++)
+                    {
+                        Console.WriteLine(numbers[i, j]);
+                    }
+                }
+
+            }
         }
     }
 }
