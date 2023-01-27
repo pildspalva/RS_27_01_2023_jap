bf7bd398f21c305ba399a1d12879a6b4d23ee70a
pildspalva <123443302+pildspalva@users.noreply.github.com>
2023-01-27 08:47:33 +0200

    Komentars 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS_27_01_2023_jap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
